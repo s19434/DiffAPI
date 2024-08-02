bad36a71ce3027331c1c602c586cc2f64d71fade
s19434 <s19434@pjwstk.edu.pl>
2024-08-02 20:29:21 +0200

    Fixed WebFactory

--- IntegrationTests/Factories/CustomWebApplicationFactory.cs
+++ IntegrationTests/Factories/CustomWebApplicationFactory.cs
@@ -1,15 +1,14 @@
-using DiffAPI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Hosting;
 
 namespace IntegrationTests.Factories;
 
-public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+public class CustomWebApplicationFactory : WebApplicationFactory<TestStartUp>
 {
     protected override IHostBuilder CreateHostBuilder()
     {
         return Host.CreateDefaultBuilder()
-            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Program>(); });
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<TestStartUp>(); });
     }
 }
